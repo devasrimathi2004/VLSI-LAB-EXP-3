a8fe126faadd2271aa36bec37c4a482a88c7a0b0
devasrimathi2004 <166363441+devasrimathi2004@users.noreply.github.com>
2024-05-14 04:57:47 +0530

    Update Exp3_Multiplier_4bit.v

--- Exp3_Multiplier_4bit.v
+++ Exp3_Multiplier_4bit.v
@@ -1,15 +1,11 @@
 module ha(a,b,sum,carry);
 input a,b;
 output sum,carry;
-
-
 endmodule
 
 module fa_ha(a,b,c,sum,carry);
 input a,b,c;
 output sum,carry;
-
-
 endmodule
 
 module multi_4(a,b,p,carry);
@@ -17,8 +13,4 @@ input[3:0]a,b;
 output [6:0]p;
 output carry;
 wire [17:1]w;
-
-
-
-
 endmodule
